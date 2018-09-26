eefc6e52c8ae8c32205f89b4628358cde980b870
elolson93 <elolson93@gmail.com>
2018-09-26 15:53:57 -0400

    Added a comment in the HelloWorld class

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -2,6 +2,7 @@ package com.codechallenge;
 
 /**
  * Created by merciersj on 8/29/2018.
+ * Updated by elolson93 on 9/26/2018
  */
 public class HelloWorld {
     public static void main(String[] args) {
