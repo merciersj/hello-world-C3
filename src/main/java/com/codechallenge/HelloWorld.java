28ba066cf70e5d3ba0687ef4e5d6637e24301bd8
elolson93 <elolson93@gmail.com>
2018-10-05 14:07:24 -0400

    Committing some changes

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -7,5 +7,163 @@ package com.codechallenge;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        
     }
+    
+    public static String beforesecond ()
+    {
+    		int i =4; 
+    			char[] a = geth();
+    			char[]b=gete();
+    	System.out.println(a[i]);
+    		System.out.println(b[i]);
+    		char[]c=getl();
+    		
+    			char[]d=getl();
+    		char[]e=geto();
+  System.out.println(c[i]);
+    		System.out.println(d[i]);
+    									System.out.println(e[i]);
+    									
+    									/*System.out.println(a[i]);
+    						    		System.out.println(b[i]);*/
+    	
+    	
+
+    		
+    	
+    }
+    
+    public static String filledwithwords ()
+    {
+    	
+    int i = 4; 
+    char [] z = gets(); 
+    System.out.println(z[i]);
+    
+    		
+    }
+    
+    public static String afterfirst ()
+    {
+    	int i =4; 
+		char[] a = getw();
+		char[]b=geto();
+System.out.println(a[i]);
+	System.out.println(b[i]);
+	char[]c=getr();
+	
+		char[]d=getl();
+	char[]e=getd();
+System.out.println(c[i]);
+	System.out.println(d[i]);
+								System.out.println(e[i]);
+								
+								/*System.out.println(a[i]);
+					    		System.out.println(b[i]);*/
+    }
+    
+    public static char[] geth ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='h';
+    		}
+    		return 1; 
+    }    
+    
+    
+    public static char[] gete ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='e';
+    		}
+    		return 1; 
+    }    
+    
+    
+    public static char[] getl ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='l';
+    		}
+    		return 1; 
+    }    
+    
+    public static char[] geto ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='o';
+    		}
+    		return 1; 
+    }    
+    
+    public static char[] getw ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='w';
+    		}
+    		return 1; 
+    }    
+    
+    
+    
+    public static char[] getr ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='r';
+    		}
+    		return 1; 
+    }    
+    
+    
+    public static char[] getd ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]='d';
+    		}
+    		return 1; 
+    }    
+    
+    public static char[] gets ()
+    {
+   
+    			char 1[] = new char[30];
+   for (int i=0; i<1.length; i++) { 
+    char[i]=' ';
+    		}
+    		return 1; 
+    }    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
