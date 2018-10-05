8fd84cf07236dfa8a2edf1252283f1ec0683efa5
Asteezy24 <alxtv1026@gmail.com>
2018-10-05 11:53:10 -0400

    Second iteration

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -14,9 +14,56 @@ public class HelloWorld {
             char j = input.charAt(i);
             charList.add(j);
         }
-        int H = 72;
+        int h = 72;
         int e = 101;
         int l = 108;
+        int o = 111;
+        int space = 32;
+        int w = 87;
+        int r = 114;
+        int l = 108;
+        int d = 100;
+        int exclamation = 33;
+
+        int k1 = 0;
+        while (k1 < h) {
+            k1+=1;
+        }
+
+        int k2 = 0;
+        while (k2 < e) {
+            k2+=1;
+        }
+
+        int k3 = 0;
+        while (k3 < l) {
+            k3+=1;
+        }
+
+        int k4 = 0;
+        while (k4 < l) {
+            k4+=1;
+        }
+
+        int k5 = 0;
+        while (k5 < o) {
+            k5+=1;
+        }
+
+        int k6 = 0;
+        while (k6 < space) {
+            k6+=1;
+        }
+
+        int k7 = 0;
+        while (k7 < w) {
+            k7+=1;
+        }
+
+        int k8 = 0;
+        while (k8 < o) {
+            k8+=1;
+        }
 
 
         System.out.println("Hello World");
