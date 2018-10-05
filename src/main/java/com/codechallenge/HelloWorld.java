c70bd29fb02495fbb4f6bf0318867ad8398bbdd8
notbrianeno <lwcolpo@gmail.com>
2018-10-05 11:04:14 -0400

    Commit Test

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -7,6 +7,7 @@ package com.codechallenge;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World"); 
+
+        System.out.println("Hello World");
     }
 }
