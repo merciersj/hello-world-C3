e7eb6b214832482a7b2997dbfb18d27548750a83
Asteezy24 <alxtv1026@gmail.com>
2018-10-05 11:47:42 -0400

    First iteration

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,10 +1,24 @@
 package com.codechallenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by merciersj on 8/29/2018.
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        String input = "Hello World";
+        List<Character> charList = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++){
+            char j = input.charAt(i);
+            charList.add(j);
+        }
+        int H = 72;
+        int e = 101;
+        int l = 108;
+
+
         System.out.println("Hello World");
     }
 }
