e27a3159873c19e3f2222a752be026b72568e7cb
notbrianeno <lwcolpo@gmail.com>
2018-10-05 14:05:28 -0400

    testing

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,4 +1,6 @@
 package com.codechallenge;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by merciersj on 8/29/2018.
@@ -7,5 +9,23 @@ package com.codechallenge;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+    }
+
+    public static boolean performGame(){
+        boolean userWin;
+        String[] names = {"lizard", "rock", "paper", "scissor", "spock"}
+        Scanner input = new Scanner(System.in);
+
+        while(input.nextLine())
+        {
+            Random num = new Random();
+            int comp= num.nextInt(4);
+            System.out.print("Enter rock, paper, scissor, lizard, or spock: ");
+            String choice = input.nextLine();
+
+        }
+
     }
 }
