74e583a8f9328cbdccaf3cd6376b267d7ee6c396
Danielle Borgerding <danielle@Danielles-MacBook-Pro-2.local>
2018-10-05 12:10:00 -0400

    initial class

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -5,6 +5,102 @@ package com.codechallenge;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        generate();
+
+
     }
-}
+
+    public static void generate()
+    {
+        String s = "asdfghjklqwertyuioplJOWJEFIWlOJfkowueWhuwfworfeijIFWJOWElFOEWHIOWJIOLKDSsJFlf.ijwofehulshfjkdsfhe";
+        int length = s.length();
+        int i =0;
+        while(i<length()) {
+            if (s.charAt(i).toUpperCase().equals('H')) {
+                out.print('H');
+
+                int j = 0;
+                while (j < length()) {
+                    if (s.charAt(j).toLowerCase().equals('e')) {
+                        out.print('e');
+
+                        int k = 0;
+                        while (k < length()) {
+                            if (s.charAt(k).toLowerCase().equals('l')) {
+                                out.print('l');
+
+                                int l = 0;
+                                while (l < length()) {
+                                    if (s.charAt(l).toLowerCase().equals('l')) {
+                                        out.print('l');
+
+                                        int m = 0;
+                                        while (m < length()) {
+                                            if (s.charAt(m).toLowerCase().equals('o')) {
+                                                out.print('o');
+
+                                                out.print(" ");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            m++;
+                        }
+                        l++;
+                    }
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+        while(i<length()) {
+            if (s.charAt(i).toUpperCase().equals('W')) {
+                out.print('W');
+
+                int j = 0;
+                while (j < length()) {
+                    if (s.charAt(j).toLowerCase().equals('o')) {
+                        out.print('o');
+                        j++;
+                        int k = 0;
+                        while (k < length()) {
+                            if (s.charAt(k).toLowerCase().equals('r')) {
+                                out.print('r');
+                                k++;
+                                int l = 0;
+                                while (l < length()) {
+                                    if (s.charAt(l).toLowerCase().equals('l')) {
+                                        out.print('l');
+                                        l++;
+                                        int m = 0;
+                                        while (m < length()) {
+                                            if (s.charAt(m).toLowerCase().equals('d')) {
+                                                out.print('d');
+                                                m++;
+                                                out.print(".");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            m++;
+                        }
+                        l++;
+                    }
+                    k++;
+                }
+                j++;
+
+            }
+            i++;
+        }
+
+                            }
+                        }
+
+
+
+
+
