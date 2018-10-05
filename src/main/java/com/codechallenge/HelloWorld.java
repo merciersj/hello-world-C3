0ebb5862e188ea83df4951b06ee3997710b1f726
Asteezy24 <alxtv1026@gmail.com>
2018-10-05 11:57:55 -0400

    Third iteration

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -14,6 +14,8 @@ public class HelloWorld {
             char j = input.charAt(i);
             charList.add(j);
         }
+        List<Character> result = new ArrayList<Character>();
+
         int h = 72;
         int e = 101;
         int l = 108;
@@ -21,51 +23,71 @@ public class HelloWorld {
         int space = 32;
         int w = 87;
         int r = 114;
-        int l = 108;
         int d = 100;
         int exclamation = 33;
 
-        int k1 = 0;
-        while (k1 < h) {
-            k1+=1;
+        for(char c: charList) {
+            int k1 = 0;
+            while (k1 < h) {
+                k1 += 1;
+            }
+
+            int k2 = 0;
+            while (k2 < e) {
+                k2 += 1;
+            }
+
+            int k3 = 0;
+            while (k3 < l) {
+                k3 += 1;
+            }
+
+            int k4 = 0;
+            while (k4 < l) {
+                k4 += 1;
+            }
+
+            int k5 = 0;
+            while (k5 < o) {
+                k5 += 1;
+            }
+
+            int k6 = 0;
+            while (k6 < space) {
+                k6 += 1;
+            }
+
+            int k7 = 0;
+            while (k7 < w) {
+                k7 += 1;
+            }
+
+            int k8 = 0;
+            while (k8 < o) {
+                k8 += 1;
+            }
+
+            int k9 = 0;
+            while (k9 < r) {
+                k9 += 1;
+            }
+
+            int k10 = 0;
+            while (k10 < l) {
+                k10 += 1;
+            }
+
+            int k11 = 0;
+            while (k11 < d) {
+                k11 += 1;
+            }
+
+            int k12 = 0;
+            while (k12 < exclamation) {
+                k12 += 1;
+            }
+
         }
-
-        int k2 = 0;
-        while (k2 < e) {
-            k2+=1;
-        }
-
-        int k3 = 0;
-        while (k3 < l) {
-            k3+=1;
-        }
-
-        int k4 = 0;
-        while (k4 < l) {
-            k4+=1;
-        }
-
-        int k5 = 0;
-        while (k5 < o) {
-            k5+=1;
-        }
-
-        int k6 = 0;
-        while (k6 < space) {
-            k6+=1;
-        }
-
-        int k7 = 0;
-        while (k7 < w) {
-            k7+=1;
-        }
-
-        int k8 = 0;
-        while (k8 < o) {
-            k8+=1;
-        }
-
-
         System.out.println("Hello World");
     }
 }
