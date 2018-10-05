b49b619642e4a96b5f8676396be7b1c5762b1d0c
Bryan Arnold <bryanhallarnold@gmail.com>
2018-10-05 14:19:45 -0400

    second push

---
+++ src/main/java/com/codechallenge/E.java
@@ -0,0 +1,8 @@
+package com.codechallenge;
+
+public class E {
+    public String GetE() {
+        String e = "e";
+        return e;
+    }
+}
--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -5,6 +5,32 @@ package com.codechallenge;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+       // System.out.println("Hello World");
+        String output = "";
+        H typeh = new H();
+        E typee = new E();
+        L typel1 = new L();
+        L typel2 = new L();
+        O typeo = new O();
+        W typeW = new W();
+        O typeo2 = new O();
+        R typer = new R();
+        L typel3 = new L();
+        D typed = new D();
+        Bang typebang = new Bang();
+        System.out.print(typeh.GetH());
+        System.out.print(typee.GetE());
+        System.out.print(typel1.GetL());
+        System.out.print(typel2.GetL());
+        System.out.print(typeo.GetO());
+        System.out.print(typeW.GetW());
+        System.out.print(typeo2.GetO());
+        System.out.print(typer.GetR());
+        System.out.print(typel3.GetL());
+        System.out.print(typed.GetD());
+        System.out.print(typebang.GetBang());
+
+
     }
 }
