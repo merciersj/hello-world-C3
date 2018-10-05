510a8d5fd5b136f4b987c72b148fe7806fd4fa6e
Trevor Bennett <github@trevorbennett.us>
2018-10-05 14:14:55 -0400

    stuff

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,10 +1,47 @@
 package com.codechallenge;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by merciersj on 8/29/2018.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	public static void main(String[] args) {
+		String h = "o";
+		String l = "W";
+		String e = "d";
+		String r = "e";
+		String o = "l";
+		String W = "H";
+		String d = "r";
+		List<String> bannana = new ArrayList<String>();
+
+		bannana.add(0, W);
+		apple(W);
+		apple(r);
+		apple(o);
+		apple(o);
+		apple(h);
+		apple(" ");
+		apple(l);
+		apple(h);
+		apple(d);
+		apple(o);
+		apple(e);
+
+
+	}
+
+	public static String apple(String index) {
+		Random shellfish = new Random();
+		int n= shellfish.nextInt(50000)+1;
+		while(n<50000){
+			n= shellfish.nextInt(50000)+1;
+		}
+		System.out.print(index);
+		return index;
+	}
 }
