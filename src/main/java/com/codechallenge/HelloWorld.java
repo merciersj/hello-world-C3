4af53d285642d0902fe90a3ba8219d6c5c8dcf2e
jamesbaker <baker.1927@osu.edu>
2018-10-05 14:01:07 -0400

    Edit

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,11 +1,42 @@
 package com.codechallenge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by merciersj on 8/29/2018.
  * Edited by James Baker on 9/26/2018
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        char a = 'H';
+        char b = 'e';
+        char c = 'l';
+        char j = 'o';
+        char k = 'r';
+        char z = 'l';
+        char x = 'd';
+        char d = 'l';
+        char e = 'o';
+        char f = ' ';
+        char i = 'W';
+
+        Map<Character, Character> bye = new HashMap<Character, Character>();
+        bye.put(a,a);
+        bye.put(b,b);
+        bye.put(c,c);
+        bye.put(d,d);
+        bye.put(e,e);
+        bye.put(f,f);
+        bye.put(i,i);
+        bye.put(j,j);
+        bye.put(k,k);
+        bye.put(z,z);
+        bye.put(x,x);
+
+      //  if(bye.key(a))
+
+            System.out.println("Hello World");
+
     }
 }
